c68a4d766bd63820f6c432921c0960e51a39c049
KristianAskS <kristians.sin.konto@gmail.com>
2025-03-17 16:43:14 +0100

    Initial commit

---
+++ src/main/java/edu/ntnu/idi/bidata/idatt2003/Main.java
@@ -0,0 +1,8 @@
+package edu.ntnu.idi.bidata.idatt2003;
+
+public class Main {
+
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+  }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,4 @@
+module edu.ntnu.idi.bidata.idatt2003 {
+  exports edu.ntnu.idi.bidata.idatt2003;
+  requires javafx.controls;
+}
